b58854118216a7b391c79edbfd92cf0ef8b12d4e
Joe Maloney <pkgdemonteam@gmail.com>
2021-02-28 00:14:33 -0500

    Be specific about files copied to avoid copying /etc/defaults/rc.conf to /etc/rc.conf

--- tests/openrc/runtests.sh
+++ tests/openrc/runtests.sh
@@ -165,7 +165,8 @@ install_repo_changes()
   cp -R ${CWD}/../../libexec/rc/etc.init.d/ /tmp/ghostbsd-openrc-ci-pool/etc/init.d/
 
   # Copy files from  ../../libexec/rc/ to /etc/ in disk image
-  cp ${CWD}/../../libexec/rc/rc.* /tmp/ghostbsd-openrc-ci-pool/etc/
+  cp ${CWD}/../../libexec/rc/rc.devd /tmp/ghostbsd-openrc-ci-pool/etc/
+  cp ${CWD}/../../libexec/rc/rc.devmatch /tmp/ghostbsd-openrc-ci-pool/etc/
   cp ${CWD}/../../libexec/rc/openrc* /tmp/ghostbsd-openrc-ci-pool/etc/  
 
   # Copy files from ../../sbin/devd/ to /etc/devd-openrc/ in disk image
